2976212aa14cdf37c9d3a21008fe2c457e9822d6
Marto19 <bigguissss@gmail.com>
2024-10-15 00:31:25 +0300

    Initial commit

---
+++ phrmSystem/src/main/java/com/phrmSystem/phrmSystem/PhrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.phrmSystem.phrmSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrmSystemApplication.class, args);
+	}
+
+}
---
+++ phrmSystem/src/test/java/com/phrmSystem/phrmSystem/PhrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phrmSystem.phrmSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
