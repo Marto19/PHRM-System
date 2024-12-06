ed376739c1a81704955df3cf93cb0c6e57333475
Marto19 <bigguissss@gmail.com>
2024-12-06 09:12:12 +0000

    Role entity created

---
+++ phrmSystem/src/main/java/com/phrmSystem/phrmSystem/data/entity/Role.java
@@ -0,0 +1,33 @@
+package com.phrmSystem.phrmSystem.data.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    @NotBlank(message = "The role name cannot be blank.")
+    @Size(min = 3, max = 20, message = "The role name cannot be below 3 and exceed 20 characters.")
+    private String role_name;
+
+    @Column(name = "description")
+    @Size(max = 100, message = "The description must not exceed 100 characters.")
+    private String description;
+
+    public Role(String role_name) {
+        this.role_name = role_name;
+    }
+}
