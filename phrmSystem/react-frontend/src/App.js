4f0dd7976a663d9bd87f6b93190950fa4e0231a8
Marto19 <bigguissss@gmail.com>
2024-12-10 20:37:38 +0000

    Initial front-end commit

--- phrmSystem/react-frontend/src/App.js
+++ phrmSystem/react-frontend/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Protected from "./component/Protected"
+import Public from "./component/Public"
+
+import useAuth from "./hooks/UserAuth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const isLoged = useAuth();
+
+  return  isLoged ? <Protected /> : <Public />;
 }
 
 export default App;
