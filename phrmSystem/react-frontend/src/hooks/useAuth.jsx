321c407bece995f6e9bf57367c7db22991eb1fef
Marto19 <bigguissss@gmail.com>
2024-12-17 12:52:26 +0000

    TOTAL REFACTOR FRONTEND
    keycloak now works with spring together

---
+++ phrmSystem/react-frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react";
+import  Keycloak  from "keycloak-js"
+
+const useAuth = () => {
+    const [isLogin, setLogin] = useState(false);
+
+    useEffect(() => {
+        const client = new Keycloak({
+           url:import.meta.env.VITE_KEYCLOAK_URL,
+           realm:import.meta.env.VITE_KEYCLOAK_REALM,
+            clientId:import.meta.env.VITE_KEYCLOAK_CLIENT,
+        });
+
+        client.init({onLoad: "login-required"}).then((res) => setLogin(res))
+    }, []);
+
+    return isLogin
+}
+
+// const useAuth = () => {
+//     const [isLogin, setLogin] = useState(false);
+//     return isLogin;
+// }
+
+export default useAuth;
